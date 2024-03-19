a3db1df392f3218e2c3bf20459b19e9e76c31354
klklklndr <klndr.ercan@gmail.com>
2024-03-20 00:45:43 +0300

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import SessionProvider from "./SessionProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Flowmazon",
+  title: "Ercü Test",
   description: "We make your wallet cry",
 };
 
